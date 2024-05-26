23bf2354350f253721c669280d5962846f78b26a
harshil <168345557+harshilvora0511@users.noreply.github.com>
2024-05-26 11:33:32 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use Strict"; // treat all JS code as newer version
+
+// alert (3+3) // we are using nodejs / not browser
+
+console.log(3+3);
+console.log("harshil");
+
+let name = "Harshil"
+let age = 25
+let isLoggedIn = false
+
+// number => 2 to power 52
+// bigInd // Jyare number vadhare motu hoy tyare bigInd use thay che
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // type undefined
+console.log(typeof null); // type object

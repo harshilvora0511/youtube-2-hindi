15efb98e68227213268bb923eaf5cd6a01b8e498
harshil <168345557+harshilvora0511@users.noreply.github.com>
2024-05-26 11:16:31 +0000

    variable new file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 545668
+let accountEmail = "harshil@hgdshfw.com"
+var accountPassword = "454564"
+accountCity = "Jaipur"
+
+// accountId = 5454 // not allowed
+
+accountEmail = "bshg@dfs.com"
+accountPassword = "2564646"
+accountCity = "India"
+
+console.log(accountId)
+
+console.table([accountEmail,accountId,accountPassword,accountCity])

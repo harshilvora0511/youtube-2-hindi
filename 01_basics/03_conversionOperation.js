228c74ed9b522058b1d58f8d05a859f59618ef73
harshil <168345557+harshilvora0511@users.noreply.github.com>
2024-05-26 11:54:16 +0000

    conversionOperation-43

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,43 @@
+let score = "33"
+let score2 = "33vvg"
+let score3 = null
+let score4 = undefined
+let score5 = true
+
+let valueInNumber = Number(score)
+let valueInNumber2 = Number(score2)
+let valueInNumber3 = Number(score3)
+let valueInNumber4 = Number(score4)
+let valueInNumber5 = Number(score5)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber2);
+// console.log(valueInNumber3);
+// console.log(valueInNumber4);
+// console.log(valueInNumber5);
+
+// "33" => 33
+// "33sdg" => NaN
+// true => 1; false => 0
+
+// ------------------------------------------------
+
+let isLoggedIn = "Harshil"
+let isLoggedIn2 = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+let booleanIsLoggedIn2 = Boolean(isLoggedIn2)
+// console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn2);
+
+// 1 => true; 0 => false
+// "" => false
+// "harshil" => true
+
+// -----------------------------------------------
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);

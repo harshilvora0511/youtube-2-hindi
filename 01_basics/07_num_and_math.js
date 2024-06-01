ae6b6211bcdf0e24d88cb261037e7f65feaabfe9
harshil <168345557+harshilvora0511@users.noreply.github.com>
2024-06-01 04:57:23 +0000

    num_and_math

---
+++ 01_basics/07_num_and_math.js
@@ -0,0 +1,31 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.5694
+// console.log(otherNumber.toPrecision(4))
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString())
+
+// ************************* Maths ************************************
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+// console.log(Math.min(4,1,8,4,89,4,8,1,))
+// console.log(Math.max(4,1,8,4,89,4,8,1,))
+
+console.log(Math.floor(Math.random() * 10) + 1)
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min))

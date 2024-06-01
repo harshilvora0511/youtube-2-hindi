a71d90435ac488d48e591c3c054acac8571d9721
harshil <168345557+harshilvora0511@users.noreply.github.com>
2024-06-01 05:02:22 +0000

    scops

---
+++ 03_basics/02_scops.js
@@ -0,0 +1,47 @@
+// let a = 300
+
+// if (true) {
+//     let a = 10;
+//     const b = 5;
+//     console.log(a);
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// function one(){
+//     const userName = "Harshil"
+
+//     function two(){
+//         const chenal = "google"
+//         console.log(userName);
+//     }
+//     // console.log(chenal);
+//     two()
+// }
+
+// one()
+
+if(true){
+    const userName = "Harshil"
+    if(userName === "Harshil"){
+        const chenal = "instagram"
+        // console.log(userName + chenal);
+    }
+    // console.log(chenal);
+}
+// console.log(userName);
+
+// ********************************** intresting ****************************************
+
+console.log(addone(5))
+function addone(num){
+    return num+1
+}
+
+
+console.log(addone2(5))
+const addone2 = function (num){
+    return num+1
+}
